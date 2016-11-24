5e11b65b415ca7c1fb717d44f774aac746d09344
Tarun Chattoraj <kumar.chattoraj@gmail.com>
2016-11-24 10:33:06 -0500

    providing home and visitor views using ng-show for home visitor tabs

--- app/scripts/controllers/scorecardCtrl.js
+++ app/scripts/controllers/scorecardCtrl.js
@@ -2,7 +2,32 @@
     function ScorecardCtrl($scope){
         this.rowsArray = [1,2,3,4,5,6,7,8,9];
         this.inningsArray = [1,2,3,4,5,6,7,8,9,10];
+    
+        $scope.showHome = true;
+        $scope.showVisitors = false;
+        
+        $scope.showHomeTeam = function() {
+            $scope.showHome = true;
+            $scope.showVisitors = false;
+            
+        }
+        
+        $scope.showVisitorTeam = function(){
+            $scope.showHome = false;
+            $scope.showVisitors = true;
+            
+        }
+    
+    
+    
+    
+    
+    
+    
+    
     }
+    
+    
     angular 
         .module('scorecardMod')
         .controller('scorecardCtrl', ['$scope', ScorecardCtrl]);
