4ee9bf7991945bec07aaafaa2e1f50415df42b39
Tarun Chattoraj <kumar.chattoraj@gmail.com>
2016-11-25 21:37:03 -0500

    adding in Red Sox team in states.js

--- dist/scripts/controllers/rowCtrl.js
+++ dist/scripts/controllers/rowCtrl.js
@@ -1,13 +1,6 @@
 (function() {
     function RowCtrl($scope, Lineup, $rootScope){
-        $scope.lineup = Lineup;
-
-
-//        this.rowsArray = [1,2,3,4,5,6,7,8,9];
-//        this.inningsArray = [1,2,3,4,5,6,7,8,9,10];
-
-//        $scope.roster = [{id: 1, label: "Rose", number: 14}, {id: 2, label: "Griffey", number: 30}, {id: 3, label: "Morgan", number: 8}, {id: 4, label: "Perez", number: 24}, {id: 5, label: "Bench", number: 5}, {id: 6, label: "Foster", number: 15}, { id: 7, label: "Geronimo", number: 20}, {id: 8, label: "Concepcion", number: 13}, {id: 9, label: "Gullet", number: 35}];
-        
+        $scope.lineup = Lineup;     
         $scope.getIndex = function(i) {
             $scope.rowIndex = i;
         }
@@ -17,10 +10,6 @@
 
         
         $scope.batterDropDownSettings = {selectionLimit: 1, externalIdProp: '', closeOnSelect: true, scrollable: true, scrollableHeight: 150, showCheckAll: false, showUncheckAll: false, dynamicTitle: false};
-        
-        //$scope.rowTest = $scope.$parent.available;
-        
-        
         
         $scope.eventListener = function(){
             
--- dist/scripts/values/states.js
+++ dist/scripts/values/states.js
@@ -45,7 +45,7 @@
     angular 
         .module('scorecardMod')
         .value('teamRoster', {
-          "reds": [
+          "Reds": [
               {id: 1, label: "Rose", number: 14}, 
               {id: 2, label: "Griffey", number: 30}, 
               {id: 3, label: "Morgan", number: 8}, 
@@ -55,6 +55,21 @@
               {id: 7, label: "Geronimo", number: 20}, 
               {id: 8, label: "Concepcion", number: 13}, 
               {id: 9, label: "Gullet", number: 35},
-              {id: 10, label: "Chattoraj", number:13}]    
+              {id: 10, label: "Armbrister", number: 33}],
+        
+        "Red Sox": [
+              {id: 1, label: "Carbo", number: 1},
+              {id: 2, label: "Doyle", number: 5},
+              {id: 3, label: "Yastrzemski", number: 8 },
+              {id: 4, label: "Fisk", number: 27},
+              {id: 5, label: "Lynn", number: 19},
+              {id: 6, label: "Petrocelli", number: 6},
+              {id: 7, label: "Evans", number: 24},
+              {id: 8, label: "Burleson", number: 7},
+              {id: 9, label: "Lee", number: 37},
+              {id: 10, label: "Cooper", number: 17}]
+        
+        
+        
             });
 })();
